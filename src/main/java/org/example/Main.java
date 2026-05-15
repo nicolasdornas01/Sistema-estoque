0f5f4c18b15fbc91147ead34046bef6d45183369
Nicolas <Nicolasdornas01@gmail.com>
2026-05-14 21:48:19 -0300

    test: testa fluxo de commit e push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
-package org.example;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de Estoque iniciado!");
+        System.out.println("Controle Financeiro - em construcao");
     }
 }
-
-
